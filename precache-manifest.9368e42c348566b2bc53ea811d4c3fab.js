b1c7a62056ea61d8f0b2305f8b9e32814c07e1f6
noob <noobie37@protonmail.com>
2021-06-10 19:31:54 +0530

    Updates

---
+++ precache-manifest.9368e42c348566b2bc53ea811d4c3fab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5fa7b50262d1c28e92e31001cb210c",
+    "url": "/indian-time/index.html"
+  },
+  {
+    "revision": "a1a0636bc930e7b7e7c7",
+    "url": "/indian-time/static/js/2.db7586e6.chunk.js"
+  },
+  {
+    "revision": "43fbbe5121b5da3e308b",
+    "url": "/indian-time/static/js/main.e3d07a9e.chunk.js"
+  },
+  {
+    "revision": "f06258f6cea7417f45ac",
+    "url": "/indian-time/static/js/runtime-main.a87bc04a.js"
+  }
+]);
